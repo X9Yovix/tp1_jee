5c00362684d746983861a1d86db202f9fd6500cf
Karim Ouazzou <karimouazzou@gmail.com>
2023-09-24 17:28:46 +0100

    creating java servlet

---
+++ src/main/java/tp1_jee/TestServlet.java
@@ -0,0 +1,38 @@
+package tp1_jee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/testServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String name = request.getParameter("name");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + name + "</h1>");
+		writer.close();
+	}
+
+}
